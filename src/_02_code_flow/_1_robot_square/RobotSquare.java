593cd55c5514f5da14b7c591f0c8d57ca663028a
rebeccavenger <vengerrebecca@gmail.com>
2023-10-27 08:53:18 -0700

    robotsquarewip commit and push

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -17,10 +17,11 @@ public class RobotSquare {
 
         // PEN.  Put the robot's pen down so it can draw, Use this command:
 		 /**     rob.penDown();    **/
+    	rob.penDown();
 
         // SPEED. Make the robot move quickly. Use this command:
 		  /**     rob.setSpeed(<speed>);    **/
- 
+    	rob.setSpeed(100);
     	// COUNT. Create an int variable that will count how many sides of the square we have drawn.
     	//        The start value will be zero because no sides have yet been drawn. Use this code:
 		  /**     int count = 0;    **/
@@ -28,10 +29,13 @@ public class RobotSquare {
 
         // DRAW.  Draw a line. Use this command:
     		  /**     rob.move(<distance>);    **/
+    	rob.move(100);
   
 
 	// TURN.  Turn 90 degrees. Use this command:
     		  /**     rob.turn(<angle>);    **/
+    	
+rob.turn(90);
 
     	
     	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
